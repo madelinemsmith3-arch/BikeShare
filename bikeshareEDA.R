3306a2fa34b9bdffff32ea7baaaf4d77d55aec28
madelinemsmith3-arch <madelinemsmith3@gmail.com>
2025-09-11 15:53:27 -0600

    Made bike predictions for Kaggle competition

--- bikeshareEDA.R
+++ bikeshareEDA.R
@@ -4,10 +4,18 @@ library(tidymodels)
 library(vroom)
 library(patchwork)
 library(DataExplorer)
+# setwd("C:\\Users\\madel\\OneDrive\\Documents\\Stat 348\\BikeShare")
 
 
-bike_train <- vroom("train.csv")
+# read in the data
+bike_train <- vroom("train.csv") %>%
+  select(-casual, -registered)
 
+bike_test <- vroom("test.csv")
+
+
+
+# EDA
 dplyr::glimpse(bike_train) 
 
 DataExplorer::plot_intro(bike_train)
@@ -37,5 +45,34 @@ plot4
 
 
 (plot1 + plot2) / (plot3 + plot4)
+
+
+
+# Linear Regression
+
+## Setup and Fit the Linear Regression Model
+my_linear_model <- linear_reg() %>% #Type of model
+  set_engine("lm") %>% # Engine = What R function to use
+  set_mode("regression") %>% # Regression just means quantitative response
+  fit(formula=count~.-datetime, data=bike_train)
+
+## Generate Predictions Using Linear Model
+bike_predictions <- predict(my_linear_model,
+                            new_data=bike_test) # Use fit to predict
+bike_predictions ## Look at the output
+
+
+# Format the Predictions for Submission to Kaggle
+kaggle_submission <- bike_predictions %>%
+bind_cols(., bike_test) %>% #Bind predictions with test data
+  select(datetime, .pred) %>% #Just keep datetime and prediction variables
+  rename(count=.pred) %>% #rename pred to count (for submission to Kaggle)
+  mutate(count=pmax(0, count)) %>% #pointwise max of (0, prediction)
+  mutate(datetime=as.character(format(datetime))) #needed for right format to Kaggle
+
+## Write out the file
+vroom_write(x=kaggle_submission, file="./LinearPreds.csv", delim=",")
+
+
 
 
